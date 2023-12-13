1da7fc07c8ede628de95ceb9ebe2cc1c8920c691
RanceCl <104018154+RanceCl@users.noreply.github.com>
2023-12-13 14:05:14 -0500

    Create 5-Longest-Palindromic-Substring.java

---
+++ 5-Longest-Palindromic-Substring.java
@@ -0,0 +1,54 @@
+/*
+Given a string s, the longest panindromic substring within 
+s was returned.
+
+The method used was to go through every character in s and 
+expand around this center character to find the longest 
+palindrome around it. Since the substring could be odd or 
+even, this was done twice for each character, once with 
+the center being the character at index i, and the second 
+time being done with the characters at index i and i+1.
+
+Once this new substring was found, it's length would be  
+compared to the currently stored longest substring.
+
+This was repeated until all characters as centers were 
+exhausted, with the final substring stored being the 
+longest in s. 
+*/
+class Solution {
+    public String longestPalindrome(String s) {
+        String longPal = "";
+        String substringi = "";
+        
+        // Loop through every character in string s to use each as a center. 
+        for(int i = 0; i < s.length(); i++) {
+            // Expand substring twice from 'i', since the palindromic substring could be odd or even
+            // Odd Palindrome, one center character at index 'i'.
+            substringi = findPalindromeSubstringFromCenter(i, i, s);
+            if (substringi.length() > longPal.length()) {
+                longPal = substringi;
+            }
+            
+            // Even Palindrome, two center characters at indices 'i' and 'i+1'.
+            substringi = findPalindromeSubstringFromCenter(i, i+1, s);
+            if (substringi.length() > longPal.length()) {
+                longPal = substringi;
+            }
+        }
+
+        return longPal;
+    }
+
+    // Expand the substring from the center index until the palidrome ends or an edge is reached.
+    private String findPalindromeSubstringFromCenter(int left, int right, String s) {
+        // Loop will continue until the string's edge is reached or the palindrome ends.
+        while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)) {
+            left--;
+            right++;
+        }
+
+        // New palindrome substring
+        return s.substring(left+1, right);
+    }
+}
