290b09f81c7d2536e4951f50b51ab5f759a264d2
RanceCl <104018154+RanceCl@users.noreply.github.com>
2024-05-03 15:50:00 -0400

    Create 15-3Sum-2.py
    My old solution to the following LeetCode problem:
    https://leetcode.com/problems/3sum/description/

---
+++ 15-3Sum-2.py
@@ -0,0 +1,48 @@
+'''
+An array of integers was given with the intention of returning all 
+triplets 
+    [nums[i], nums[j], nums[k]] 
+
+such that: 
+    i != j, i != k, and j != k, and 
+    nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+Assumptions: 
+3 <= nums.length <= 3000
+-105 <= nums[i] <= 105
+
+'''
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        numsLen = len(nums)
+        triples = set()
+        
+        # Two sum
+        for i in range(numsLen-2):
+            # Skip duplicates
+            if i > 0 and nums[i] == nums[i-1]: continue
+
+            x = nums[i]
+            # Two pointers from each side of the list
+            j = i+1
+            k = numsLen-1
+
+            while j < k:
+                # x + y + z = 0 --> z = -x + -y
+                y, z = nums[j], nums[k]
+                currSum = x + y + z
+
+                # Close in until a new potential 0 sum is found.
+                if currSum > 0: k -= 1
+                elif currSum < 0: j += 1
+                else:
+                    triples.add((x,y,z))
+                    j, k = j+1, k-1
+                    # Skip duplicates
+                    while j < k and nums[j] == nums[j-1]: j += 1
+                    while j < k and nums[k] == nums[k+1]: k -= 1
+        
+        return triples
