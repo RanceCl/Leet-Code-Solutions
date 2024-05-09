6b2287c8a590af65c96a348de68366ab7847b6d8
RanceCl <104018154+RanceCl@users.noreply.github.com>
2024-05-09 13:04:59 -0400

    Update 18-4Sum.py
    Separated one line if and while commands to be more in line with Python coding styles.

--- 18-4Sum.py
+++ 18-4Sum.py
@@ -27,8 +27,6 @@ class Solution:
     # Method that starts the kSum algorithm
     def kSum(self, nums, k, target):
         nums.sort()
-        
-        # Initialize the recursive loop
         return self.kSumLoop(nums, k, target)
         
     # Recursive loop to find every unique set of values that will equal the target
@@ -36,11 +34,13 @@ class Solution:
         res = []
 
         # Base case for k-sum, the 2-sum
-        if k == 2: return self.twoSumII(nums, target)
+        if k == 2: 
+            return self.twoSumII(nums, target)
         
         for i in range(len(nums)):
             # Skip duplicate values encountered
-            if i > 0 and nums[i] == nums[i-1]: continue
+            if i > 0 and nums[i] == nums[i-1]: 
+                continue
             
             solns = self.kSumLoop(nums[i+1:], k-1, target - nums[i])
             # Append the previous values for the sum to the 2-sum found
@@ -57,13 +57,16 @@ class Solution:
         # Loop through the list to find the addends
         while(l<r):
             # Current sum is smaller than target, increase lower addend
-            if nums[l] + nums[r] < target: l+=1
+            if nums[l] + nums[r] < target: 
+                l+=1
             # Current sum is larger than target, decrease higher addend
-            elif nums[l] + nums[r] > target: r-=1
+            elif nums[l] + nums[r] > target: 
+                r-=1
             # When an answer is found: 
             else:
                 res.append([nums[l], nums[r]])
                 l +=1
                 # Keep incrementing if duplicates are still found
-                while l<r and nums[l] == nums[l-1]: l +=1
+                while l<r and nums[l] == nums[l-1]: 
+                    l +=1
         return res
